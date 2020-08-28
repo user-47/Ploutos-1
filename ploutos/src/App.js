4807925a570a8e4cfc6dcd32fd54c0178ac32da8
ibk_temi <ibk12mails@gmail.com>
2020-08-28 18:55:30 +0100

    remove arrows from number input type

--- ploutos/src/App.js
+++ ploutos/src/App.js
@@ -3,6 +3,7 @@ import { Switch, Route, BrowserRouter } from 'react-router-dom';
 import LandingPage from './pages/LandingPage/LandingPage';
 import HomePage from './pages/HomePage/HomePage';
 import SignUpAuthenticationPage from './pages/SignUpAuthenticationPage/SignUpAuthenticationPage';
+import './App.css'
 
 const App = () => {
   return (
