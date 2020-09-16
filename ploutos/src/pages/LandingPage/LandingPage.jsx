cfc2d8681148c6e9b86deb6233470b2ade295179
ibk_temi <ibk12mails@gmail.com>
2020-09-16 18:17:11 +0100

    implementing new landing page design

--- ploutos/src/pages/LandingPage/LandingPage.jsx
+++ ploutos/src/pages/LandingPage/LandingPage.jsx
@@ -6,48 +6,104 @@ import './LandingPage.styles.css';
 
 
 const LandingPage = () => {
-  const [modalShow, setModalShow] = React.useState(false);
-  const handleModalClose = () =>{
-    setModalShow(false)
-  }
-
+  
   return (
     <div>
       <Header />
-      <div className="landing-page-body">
-        <div className="landing-page-container">
-          <div className="landing-page-main-caption">
-            <div>EXCHANGE</div>
-            <div>NAIRA <img className="nation-flag" src="../../assets/images/ngnflag.png" alt="nigerian flag"/></div>
-            <div>AND CAD <img className="nation-flag" src="../../assets/images/CADflag.png" alt="canadian flag"/></div>
-          </div>
-          <div className="landing-page-subtext">
-            Define your own rates <br/>
-            Fast and Effortless <br/>
-            Trusted network <br/>
-            Competitive rates <br/>
-          </div>
-          <Button 
-            variant="danger"
-            className="join-now-btn"
-            onClick={() => setModalShow(true)}
+      {/* start banner section */}
+      <div>
+        <div 
+          style={{
+            width: '100%',
+            height: '850px',
+            backgroundImage: "url('../../assets/images/bgImage.png')",
+            backgroundRepeat: 'no-repeat',
+            backgroundSize: 'cover',
+            paddingTop: '6%'
+          }}
+        >
+          <div
+            style={{
+              marginTop: '5%',
+              marginLeft: '6%',
+              height: '75%',
+              width: '48%',
+              backgroundColor: 'rgba(0, 0, 0, 0.5)',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              borderRadius: '45% 70% 39% 50%',
+            }}
           >
-            Join Now
-          </Button>
-          <SignUpModal 
-            show={modalShow}
-            onHide={() => setModalShow(false)}
-            closeOnSubmit={handleModalClose}
-          />
+            <div
+              style={{
+                color: 'white',
+                marginLeft: '8%'
+              }}
+            >
+              <div style={{fontSize: '85px', lineHeight: '0.9', fontWeight: 'bold'}}>
+                EXCHANGE <br/>
+                NAIRA <br/>
+                AND CAD <br/>
+              </div>
+              <p>Choose your own Exchange Rate</p>
+            </div>
+          </div>
         </div>
-        <div className="landing-page-secondary-bg-image"></div>
       </div>
-      <div className="landing-page-footer">
-        <div className="landing-page-footer-links">About</div>
-        <div className="landing-page-footer-links">How it works</div>
-        <div className="landing-page-footer-links">Terms and Conditions</div>
-        <div className="landing-page-footer-links">Contact</div>
-      </div>
+      {/* end banner section */}
+      <section
+        style={{
+          padding: '40px'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            width: '100%'
+          }}
+        >
+          <div 
+            style={{
+              width: '50%',
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <div
+              style={{
+                height: '400px',
+                width: '400px',
+                borderRadius: '50%',
+                backgroundColor: '#6BA8F8'
+              }}
+            >
+              <div
+                style={{
+                  height: '200px',
+                  width: '200px',
+                  borderRadius: '50%',
+                  backgroundColor: '#6BA8F8',
+                  float: 'right',
+                  marginRight: '-7%',
+                }}
+              >
+                
+              </div>
+              {/* <img src="../../assets/images/Laptop_mockup.png" /> */}
+              <div 
+                style={{
+                  backgroundImage: "url('../../assets/images/laptop.png')",
+                  backgroundSize: 'contain',
+                  height: '350px',
+                  width: '375px'
+                }}
+              ></div>
+            </div>
+          </div>
+          <div style={{width: '50%'}}>yyyyxxxxx</div>
+        </div>
+      </section>
     </div>
   )
 }
