d105890e03adc721eadce709d3f878819971fb93
ibk_temi <ibk12mails@gmail.com>
2020-10-13 19:30:28 +0100

    working on responsive registration page

--- ploutos/src/pages/RegistrationPage/RegistrationPage.jsx
+++ ploutos/src/pages/RegistrationPage/RegistrationPage.jsx
@@ -8,6 +8,7 @@ import { Link } from 'react-router-dom';
 const RegistrationPage = () => {
   return (
     <div
+      className="reg-page"
       style={{ 
         width: '100%', 
         height: '100vh', 
@@ -21,6 +22,7 @@ const RegistrationPage = () => {
       }}
     >
       <div
+        className='reg-container'
         style={{
           width: '75%',
           height: '75%',
@@ -31,6 +33,7 @@ const RegistrationPage = () => {
         }}
       >
         <div
+          className='reg-container-img-section'
           style={{
             width: '32%',
             height: '100%',
@@ -44,29 +47,30 @@ const RegistrationPage = () => {
           <Link to='/'>
             <img
               src="../../assets/images/temp_logo.svg"
-              width="95"
-              height="85"
-              className="d-inline-block align-top"
+              // width="95"
+              // height="85"
+              className="d-inline-block align-top logo-img"
               alt="logo"
-              style={{ position: 'fixed'}}
+              style={{ position: 'fixed', width: '95px', height: '85px' }}
             />
           </Link>
-        <div
-          style={{
-            height: '100%',
-            display: 'flex',
-            justifyContent: 'center'
-          }}
-        >
-          <img
-            src={require('../../assets/images/registration_page_img1.png')}
-            className="d-inline-block align-top"
-            alt="image"
-            style={{ width: '380px', height: '300px', marginTop: '32%'}}
-          />
+          <div
+            style={{
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center'
+            }}
+          >
+            <img
+              src={require('../../assets/images/registration_page_img1.png')}
+              className="d-inline-block align-top reg-container-img"
+              alt="image"
+              style={{ width: '380px', height: '300px', marginTop: '32%'}}
+            />
+          </div>
         </div>
-        </div>
         <div
+          className="reg-form-section"
           style={{
             width: '68%',
             height: '100%',
@@ -77,7 +81,7 @@ const RegistrationPage = () => {
           }}
         >
           <div style={{ color: '#0D4C9E'}}>
-            <img src={require('../../assets/icons/heading-icon.svg')} alt='icon' className='mt-2 mb-2' style={{width: '28px', height: '28px'}} />
+            <img src={require('../../assets/icons/heading-icon.svg')} alt='icon' className='mt-2 mb-2 title-icon' style={{width: '28px', height: '28px'}} />
             <div style={{ fontSize: '30px'}}><b>REGISTRATION</b></div>
             <p style={{ fontSize: '12px' }}>Already have an account? <Link to='/login' style={{ color: '#0D4C9E', textDecoration: 'none'}}><b>Login here</b></Link></p>
           </div>
@@ -93,28 +97,28 @@ const RegistrationPage = () => {
               <Grid container spacing={2}>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="First Name" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Last Name" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Email Address" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Username" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
@@ -129,14 +133,14 @@ const RegistrationPage = () => {
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Confirm Password" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Country"
                     type='select'
                     className="registration-form-textfield mt-3 mb-3" 
@@ -144,22 +148,23 @@ const RegistrationPage = () => {
                 </Grid>
                 <Grid item xs={6}>
                   <div
+                    className='state-province-container'
                     style={{
                       display: 'flex',
                       width: '90%',
                     }}
                   >
-                    <div style={{width: '50%'}}>
+                    <div className='state-province-field' style={{width: '50%'}}>
                       <TextField 
-                        id="standard-basic" 
+                        // id="standard-basic" 
                         label="State/Province"
                         type='select'
                         className="registration-form-textfield mt-3 mb-3" 
                       />
                     </div>
-                    <div style={{width: '50%'}}>
+                    <div className='state-province-field' style={{width: '50%'}}>
                       <TextField 
-                        id="standard-basic" 
+                        // id="standard-basic" 
                         label="City"
                         type='select'
                         className="registration-form-textfield mt-3 mb-3" 
@@ -169,17 +174,17 @@ const RegistrationPage = () => {
                 </Grid>
                 <Grid item xs={6}>
                   <TextField 
-                    id="standard-basic" 
+                    // id="standard-basic" 
                     label="Phone Number" 
                     className="registration-form-textfield mt-3 mb-3" 
                   />
                 </Grid>
               </Grid>
               
-              <div style={{width: '92%'}}>
+              <div className='button-container' style={{width: '92%'}}>
                 <Button 
                   variant="outlined"
-                  className=""
+                  className="finish-button"
                   // onClick={() => onContactFormSubmission()}
                   style={{
                     borderRadius: '25px',
